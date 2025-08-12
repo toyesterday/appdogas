28d412b130afb210a38636a591817dc40feeab61
[dyad] <blogcamisasdetime@gmail.com>
2025-08-12 13:49:10 -0300

    [dyad] Fixing React Router layout error - wrote 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar.tsx';
 import BottomNav from './BottomNav.tsx';
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-const Layout = ({ children }: LayoutProps) => {
+const Layout = () => {
   return (
     <div className="min-h-screen bg-gray-50 font-sans">
       <Navbar />
-      <main className="pb-24 pt-16">{children}</main>
+      <main className="pb-24 pt-16">
+        <Outlet />
+      </main>
       <BottomNav />
     </div>
   );
